9f5510f608aa5d3d72559362cd686dfb5f59a6fb
Eduardo <eduardo78d@gmail.com>
2019-05-31 21:59:58 -0500

    labels values

--- const.py
+++ const.py
@@ -9,3 +9,5 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ICON_DIR = os.path.join(BASE_DIR, 'resources')
 
 BTC_ICON = os.path.join(ICON_DIR, 'btc.png')
+PADDING_VALUES = (WIDTH / 2) * 0.09
+PADDING_BOTTOM_VAL = 20
--- main.py
+++ main.py
@@ -19,19 +19,20 @@ class BTC(toga.App):
         self.create_price_label()
         self.create_currency_label()
         self.create_price_elements_label()
+        self.create_price_elements_value_label()
 
     def create_image(self):
         image = toga.Image(BTC_ICON)
         self.logo = toga.ImageView(image=image, style=Pack(
             width=150, height=150,
-            padding_top=20
+            padding_top=25, padding_bottom=25
         ))
 
     def create_price_label(self):
         self.price = toga.Label('$169,996.99', style=Pack(
             font_family=MONOSPACE, font_size=24,
             font_weight=BOLD,
-            padding_top=40, text_align=CENTER
+            text_align=CENTER
         ))
 
     def create_currency_label(self):
@@ -41,7 +42,30 @@ class BTC(toga.App):
                                     text_align=CENTER, color='GREEN')
 
     def create_price_elements_label(self):
-        pass
+        style = Pack(color='GREEN', alignment=CENTER, font_size=16, padding_left=40, padding_bottom=5)
+
+        self.bid = toga.Label('Bid', style=style)
+        self.ask = toga.Label('Ask', style=style)
+        self.low = toga.Label('Low', style=style)
+        self.high = toga.Label('High', style=style)
+        self.volume = toga.Label('24 H Volume', style=style)
+        self.spread = toga.Label('Spread', style=style)
+
+        self.volume.style.update(padding_left=10)
+        self.spread.style.update(padding_left=30)
+
+    def create_price_elements_value_label(self):
+        style = Pack(color='BLACK', alignment=CENTER, font_size=18, padding_bottom=PADDING_BOTTOM_VAL)
+        #120
+        self.bid_val = toga.Label('$169,003.39', style=style)
+        self.ask_val = toga.Label('$169,899.82', style=style)
+        self.low_val = toga.Label('$161,500.00', style=style)
+        self.high_val = toga.Label('$170,960.87', style=style)
+        self.volume_val = toga.Label('207.76', style=style)
+        self.spread_val = toga.Label('0.53%', style=style)
+
+        self.volume_val.style.update(padding_left=23)
+        self.spread_val.style.update(padding_left=30)
 
     def startup(self):
         self.main_window = toga.MainWindow('main', title=self.title, size=self.size)
@@ -51,7 +75,22 @@ class BTC(toga.App):
                 direction=COLUMN, alignment=CENTER
         ))
 
-        bottom_container = toga.Box()
+        bottom_container = toga.SplitContainer()
+        right = toga.Box(
+            children=[self.bid, self.bid_val, self.low, self.low_val, self.volume, self.volume_val],
+            style=Pack(
+                direction=COLUMN,
+                padding_left=40, padding_top=20
+            )
+        )
+        left = toga.Box(
+            children=[self.ask, self.ask_val, self.high, self.high_val, self.spread, self.spread_val],
+            style=Pack(
+                direction=COLUMN,
+                padding_left=40, padding_top=20
+            )
+        )
+        bottom_container.content = [right, left]
 
         split = toga.SplitContainer(direction=toga.SplitContainer.HORIZONTAL)
         split.content = [top_container, bottom_container]
